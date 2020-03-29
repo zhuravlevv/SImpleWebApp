903205f329e297ae0d4876ac432e72b3bef485e5
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-03-29 18:16:34 +0300

    add date picker, add drop-down list of genders

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $.getJSON('http://localhost:8080/employee', function(json) {
         let tr=[];
         for (let i = 0; i < json.length; i++) {
@@ -40,12 +41,12 @@ $(document).ready(function() {
             }),
             cache: false,
             success: function(result) {
-                $("#msg").html( "<span style='color: green'>Company added successfully</span>" );
-                window.setTimeout(function(){location.reload()},1000)
+                $("#msg").html( "<span style='color: green'>Employee added successfully</span>" );
+                window.setTimeout(function(){location.reload()},2000)
             },
             error: function(err) {
-                $("#msg").html( "<span style='color: red'>Name is required</span>" );
-                window.setTimeout(function(){location.reload()},1000)
+                $("#msg").html( "<span style='color: red'>Error</span>" );
+                window.setTimeout(function(){location.reload()},2000)
             }
         });
     });
@@ -86,12 +87,15 @@ $(document).ready(function() {
         let dateOfBirth = parent.children("td:nth-child(7)");
         let buttons = parent.children("td:nth-child(8)");
 
-        firstName.html("<input type='text' id='txtName' value='" + firstName.html() + "'/>");
-        lastName.html("<input type='text' id='txtName' value='" + lastName.html() + "'/>");
-        departmentId.html("<input type='text' id='txtName' value='" + departmentId.html() + "'/>");
-        jobTitle.html("<input type='text' id='txtName' value='" + jobTitle.html() + "'/>");
-        gender.html("<input type='text' id='txtName' value='" + gender.html() + "'/>");
-        dateOfBirth.html("<input type='text' id='txtName' value='" + dateOfBirth.html() + "'/>");
+        firstName.html("<input type='text' id='firstName' value='" + firstName.html() + "'/>");
+        lastName.html("<input type='text' id='lastName' value='" + lastName.html() + "'/>");
+        departmentId.html("<input type='text' id='departmentId' value='" + departmentId.html() + "'/>");
+        jobTitle.html("<input type='text' id='jobTitle' value='" + jobTitle.html() + "'/>");
+        gender.html("<select id='gender'>\n" +
+            "                <option>MALE</option>\n" +
+            "                <option>FEMALE</option>\n" +
+            "            </select>");
+        dateOfBirth.html("<input type='date' min='1900-01-01' max='2003-01-01' id='dateOfBirth' value='" + dateOfBirth.html() + "'/>");
         buttons.html("<button id='save'>Save</button>&nbsp;&nbsp;<button class='delete' id='" + employeeId.html() + "'>Delete</button>");
     });
 
@@ -112,30 +116,31 @@ $(document).ready(function() {
             contentType: "application/json; charset=utf-8",
             url: "http://localhost:8080/employee/" + employeeId.html(),
             data: JSON.stringify({
-                'employeeId' : employeeId.html(),
-                'firstName' : firstName.children("input[type=text]").val(),
-                'lastName' : lastName.children("input[type=text]").val(),
-                'departmentId' : departmentId.children("input[type=text]").val(),
-                'jobTitle' : jobTitle.children("input[type=text]").val(),
-                'gender' : gender.children("input[type=text]").val(),
-                'dateOfBirth' : dateOfBirth.children("input[type=text]").val()
+                'employeeId': employeeId.html(),
+                'firstName': firstName.children("input[type=text]").val(),
+                'lastName': lastName.children("input[type=text]").val(),
+                'departmentId': departmentId.children("input[type=text]").val(),
+                'jobTitle': jobTitle.children("input[type=text]").val(),
+                'gender': gender.children("select").val(),
+                'dateOfBirth': dateOfBirth.children("input[type=date]").val()
             }),
             cache: false,
-            success: function() {
+            success: function () {
                 firstName.html(firstName.children("input[type=text]").val());
                 lastName.html(lastName.children("input[type=text]").val());
                 departmentId.html(departmentId.children("input[type=text]").val());
                 jobTitle.html(jobTitle.children("input[type=text]").val());
-                gender.html(gender.children("input[type=text]").val());
-                dateOfBirth.html(dateOfBirth.children("input[type=text]").val());
+                gender.html(gender.children("select").val());
+                dateOfBirth.html(dateOfBirth.children("input[type=date]").val());
                 buttons.html("<button class='edit' id='" + employeeId.html() + "'>Edit</button>&nbsp;&nbsp;<button class='delete' id='" + employeeId.html() + "'>Delete</button>");
             },
-            error: function() {
-                $('#err').html('<span style=\'color:red; font-weight: bold; font-size: 30px;\'>Error updating record').fadeIn().fadeOut(4000, function() {
+            error: function () {
+                $('#err').html('<span style=\'color:red; font-weight: bold; font-size: 30px;\'>Error updating record').fadeIn().fadeOut(4000, function () {
                     $(this).remove();
                 });
             }
         });
+
     });
 
 });
